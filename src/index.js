5d86a9702eab16681f0a35a21bca6de873b11e28
fanzhongrong <50786543+fanzhongrong@users.noreply.github.com>
2021-09-18 10:56:25 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,30 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
+'use strict';
+var chalk = require('chalk');
+// Node
+var currentNodeVersion = process.versions.node; 
+var semver = currentNodeVersion.split('.');
+var major = semver[0];// 
+// 4.x
+if (major < 4) {
+  console.error(
+    chalk.red(
+      'You are running Node ' +
+        currentNodeVersion +
+        '.\n' +
+        'Create React App requires Node 4 or higher. \n' +
+        'Please update your version of Node.'
+    )
+  );
+  process.exit(1);
+}
+//n4
+require('./createReactApp');
+
+
+/ If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
